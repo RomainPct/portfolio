d351f01440f2c23603065a8aec560faa23d15e99
RomainPct <romain.penchenat@gmail.com>
2017-08-27 21:55:53 +0200

    AJout framework flexboxgrid

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="utf-8">
     <title> Romain Penchenat - UI/UX Designer</title>
 
-    <link rel="stylesheet" src="style/style.min.css">
+    <link rel="stylesheet" href="style/flexboxgrid/dist/flexboxgrid.min.css">
+    <link rel="stylesheet" href="style/style.min.css">
   </head>
   <body>
+
 
       <script type="text/javascript" src="script/jQuery/jquery-3.2.1.min.js"></script>
       <script type="text/javascript" src="script/bez-master/src/jquery.bez.min.js"></script>
