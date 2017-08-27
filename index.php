3fe4763f9aa29788228d183f65fb984976abb4dc
RomainPct <romain.penchenat@gmail.com>
2017-08-27 21:21:54 +0200

    Ajout jQuery

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title> Romain Penchenat - UI/UX Designer</title>
+
+    <link rel="stylesheet" src="style/style.min.css">
   </head>
   <body>
 
+      <script type="text/javascript" src="script/jQuery/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="script/bez-master/src/jquery.bez.min.js"></script>
+      <script type="text/javascript" src="script/script.min.js"></script>
   </body>
 </html>
