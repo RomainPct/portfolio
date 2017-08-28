b6778087abd6baadabdd6e723e2536ec8fddd090
RomainPct <romain.penchenat@gmail.com>
2017-08-28 08:44:52 +0200

    Test du framework flexboxgrid

--- index.php
+++ index.php
@@ -4,28 +4,61 @@
     <meta charset="utf-8">
     <title> Romain Penchenat - UI/UX Designer</title>
 
-    <link rel="stylesheet" href="style/flexboxgrid/dist/flexboxgrid.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,500,700" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="style/flexboxgrid/href/normalize.min.css"> -->
+    <!-- <link rel="stylesheet" href="style/flexboxgrid/href/flexboxgrid.min.css"> -->
+    <link rel="stylesheet" href="style/simpleGrid/href/simpleGrid.min.css">
+    <link rel="stylesheet" href="style/fontSize.min.css">
     <link rel="stylesheet" href="style/style.min.css">
   </head>
   <body>
 
 <div class="large-space"></div>
-<div class="row">
-    <div class="col-md-6 col-xs-12">
+<!-- <div class="row middle-xs">
+    <div class="col-md-6 col-sm-8 col-xs-12">
         <div class="row middle-xs">
-            <div class="col-xs-4"><div class="box classic">Logo</div></div>
+            <div class="col-xs-4"><div class="box image">
+                <img src="ressources/general/logo.png" alt="logo">
+            </div></div>
             <div class="col-xs-8">
-                <div class="box invisible">Romain <br> Penchenat</div>
+                <div class="box invisible"><h1 class="headache">Romain <br> Penchenat</h1></div>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-12">
-                <div class="box classic">UX - UI Designer</div>
+                <div class="box classic center-xs"><h1>UX - UI Designer</h1></div>
             </div>
         </div>
     </div>
-    <div class="col-md-6 col-xs-12">
-        <div class="box image"></div>
+    <div class="col-md-6 col-sm-4 col-xs-12">
+        <div class="box image">
+            <img src="ressources/general/profil.jpg" alt="Romain Penchenat UX-UI Designer">
+        </div>
+    </div>
+</div> -->
+
+<div class="row">
+    <div class="col xs-6">
+        <div class="row">
+            <div class="col xs-4">
+                <div class="box image">
+                    <img src="ressources/general/logo.png" alt="logo">
+                </div>
+            </div>
+            <div class="col xs-8">
+                <h1 class="headache">Romain <br>Penchenat</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col xs-12">
+                <div class="box classic">
+                    <h1>UX - UI Design</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col xs-6">
+            <div class="box image"><img src="ressources/general/profil.jpg" alt=""></div>
     </div>
 </div>
 
